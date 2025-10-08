b7eed15f84589d1ea355447b905dc34612f018b4
shubh-codes7 <shubhamcodes7@gmail.com>
2025-10-08 11:57:02 +0530

    sign in page removed

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from '@clerk/nextjs'
-
-export default function Page() {
-  return (
-    <div className='w-full h-[100vh] flex items-center justify-center'>
-      <SignIn />
-    </div>
-  )
-}
